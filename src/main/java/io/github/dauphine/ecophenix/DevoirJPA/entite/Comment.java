359207be5210c18150329ca69f82d5e2538e27e2
Dimitry19 <orlynn1anais>
2019-02-25 23:04:25 +0100

    Creation des deux servlets ,de l'entité et du file persistence.xml. la Database est encore à configurer

---
+++ src/main/java/io/github/dauphine/ecophenix/DevoirJPA/entite/Comment.java
@@ -0,0 +1,52 @@
+package io.github.dauphine.ecophenix.DevoirJPA.entite;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="COMMENT", schema="PUBLIC")
+
+	@NamedQuery(name="Comment.findAll", query="SELECT c FROM Comment c")
+public class Comment {
+	  
+	   private int id;
+	   private String description;
+	   private Timestamp date;
+
+	@Id @GeneratedValue
+	@Column(name = "id")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	   @Column(name="DESCRIPTION")
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Column(name="TIMESTAMP")
+	public Timestamp getDate() {
+		return date;
+	}
+
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
+	   
+	   
+
+}
---
+++ src/main/java/io/github/dauphine/ecophenix/DevoirJPA/ws/GetComment.java
@@ -0,0 +1,39 @@
+package io.github.dauphine.ecophenix.DevoirJPA.ws;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import io.github.dauphine.ecophenix.DevoirJPA.BO.CommentBO;
+ 
+@WebServlet(
+        name = "GetCommentServlet",
+        description = "Retourne les commentaires",
+        urlPatterns = "/comments/all"
+)
+public class GetComment extends HttpServlet {
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+	@SuppressWarnings("unused")
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	            throws IOException {
+			 
+		 try
+			{
+			 List comments=CommentBO.getInstance().allComment();
+			 
+			 response.getWriter().println("Hello");
+			}
+			catch (Exception e){
+					}
+	    }
+}
